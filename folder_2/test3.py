837c3eced115941c6ba15a48b068f24dc99133d6
beybars1 <ck002.ck002@gmail.com>
2024-06-06 22:50:03 +0500

    first commit

---
+++ folder_2/test3.py
@@ -0,0 +1,7 @@
+import numpy as np
+import os
+
+if __name__ == '__main__':
+    print('Hello, world!')
+    print('Current directory:', os.getcwd())
+    print('Random number:', np.random.randn(9,10))
